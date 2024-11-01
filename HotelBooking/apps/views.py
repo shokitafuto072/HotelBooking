f1a5f6373c0e4e697cf5a39ba3579c25283c33c6
futoShokita <fko2347090@stu.o-hara.ac.jp>
2024-11-01 14:58:31 +0900

    文字が出ます

--- HotelBooking/apps/views.py
+++ HotelBooking/apps/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 ############################私たちは総じておまんこが大好きです。############################
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world!")
