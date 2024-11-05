606e5b76892a1333aee267dae85e75218418a4be
KaitoIchino <fko2347094@stu.o-hara.ac.jp>
2024-11-05 12:07:47 +0900

    oooo

--- HotelBooking/apps/views.py
+++ HotelBooking/apps/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-############################私たちは総じておまんこが大好きです。############################
+########################################################
 # Create your views here.
 
 def index(request):
