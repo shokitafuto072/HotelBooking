a6b82f699549a5c017d8cea2b77d259178f92fa3
futoShokita <fko2347090@stu.o-hara.ac.jp>
2024-11-05 10:48:08 +0900

    feignow”

--- HotelBooking/apps/views.py
+++ HotelBooking/apps/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 ############################私たちは総じておまんこが大好きです。############################
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    template = loader.get_template("apps/index.html")
+    return HttpResponse(template.render({}, request))
