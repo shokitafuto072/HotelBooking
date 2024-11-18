4022af82419f9c23d7b8df5ca8b0566caa2b6d95
futoShokita <fko2347090@stu.o-hara.ac.jp>
2024-11-18 15:08:01 +0900

    haha

--- HotelBooking/apps/views.py
+++ HotelBooking/apps/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
 
-############################私たちは総じておまんこが大好きです。###########################
 # Create your views here.
 
 def index(request):
