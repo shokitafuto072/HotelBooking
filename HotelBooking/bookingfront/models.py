66dfb5e9e11726b1fc5bd873f637707b8311db09
futoShokita <fko2347090@stu.o-hara.ac.jp>
2024-11-22 09:35:28 +0900

    aaa

--- HotelBooking/bookingfront/models.py
+++ HotelBooking/bookingfront/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Reservation(models.Model):
+    roomtype=models.CharField("部屋タイプ",max_length=30)
+    Reservation_date=models.DateField("予約日付",auto_now=True)
+
+
 # Create your models here.
--- HotelBooking/bookingfront/views.py
+++ HotelBooking/bookingfront/views.py
@@ -11,6 +11,12 @@ def planselection(request):
 def bookingmanage(request):
     return render(request,'bookingmanage.html')
 
+def form_page(request):
+    if request.method == 'POST':
+        data = request.POST.get('data')
+        return render(request, 'next_page.html', {'data': data})
+    return render(request, 'form_page.html')
+
 
 
 
